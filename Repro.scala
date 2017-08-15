56fcdda9b5bb6d3454dc3489256b52e61136b583
Anatoly Fayngelerin <anatoly@dwnld.me>
2017-08-15 15:37:04 -0400

    some comments

--- Repro.scala
+++ Repro.scala
@@ -3,8 +3,22 @@ import com.twitter.finagle.Http
 
 object Repro {
   def main(args: Array[String]): Unit = {
-    val f = Http.Client()
-    Http.client.newService("http://google.com:80")
+    val f = Http.Client().newService("google.com:80")
+// at this point, we've hit this situation:
+// main[1] dump this
+//  this = {
+//     MODULE$: instance of com.twitter.finagle.Http$Client$(id=3318)
+//     stack: null
+// }
+// main[1] where
+//   [1] com.twitter.finagle.Http$Client$.stack (Http.scala:147)
+//   [2] com.twitter.finagle.Http$Client$.apply$default$1 (Http.scala:164)
+//   [3] com.twitter.finagle.Http$.<init> (Http.scala:288)
+//   [4] com.twitter.finagle.Http$.<clinit> (null)
+//   [5] com.twitter.finagle.Http$Client$.<init> (Http.scala:154)
+//   [6] com.twitter.finagle.Http$Client$.<clinit> (null)
+// This will NPE for the rest of time
+    Http.client.newService("google.com:80")
 
   }
 }
